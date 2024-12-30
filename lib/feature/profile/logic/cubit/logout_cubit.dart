3cf3fbf3b33c66566d696e3068c1be07c64fbb45
DESKTOP-FH6ANST\Bakrow <mohamedelbakrawy70@gmail.com>
2024-12-30 11:56:35 +0200

    - finish signout function

--- lib/feature/profile/logic/cubit/logout_cubit.dart
+++ lib/feature/profile/logic/cubit/logout_cubit.dart
@@ -1,8 +1,25 @@
+
+import 'dart:developer';
+
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 part 'logout_state.dart';
 
 class LogoutCubit extends Cubit<LogoutState> {
   LogoutCubit() : super(LogoutInitial());
+
+  SupabaseClient client = Supabase.instance.client;
+
+  Future<void> logout() async {
+    emit(LogoutLoading());
+    try {
+      await client.auth.signOut();
+      emit(LogoutSuccess());
+    } catch (e) {
+      log(e.toString());
+      emit(LogoutFailed(e.toString()));
+    }
+  }
 }
